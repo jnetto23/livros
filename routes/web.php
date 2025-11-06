a98f40c4f8c070bacacc753a827c645993c07e44
Joao Netto <jnetto@fyyb.com.br>
2025-11-06 10:36:09 -0300

    chore: rotas em ptbr

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use App\Livewire\HomePage;
 use App\Http\Controllers\ReportController;
 
 // "/" redireciona para /subjects (opcional)
-Route::redirect('/', '/subjects');
+Route::redirect('/', '/livros');
 
 // Rotas amigáveis, todas apontando pro mesmo componente, mudando "section"
-Route::get('/subjects', HomePage::class)->name('subjects')->defaults('section', 'subjects');
-Route::get('/authors',  HomePage::class)->name('authors')->defaults('section', 'authors');
-Route::get('/books',    HomePage::class)->name('books')->defaults('section', 'books');
+Route::get('/assuntos', HomePage::class)->name('subjects')->defaults('section', 'subjects');
+Route::get('/autores',  HomePage::class)->name('authors')->defaults('section', 'authors');
+Route::get('/livros',    HomePage::class)->name('books')->defaults('section', 'books');
 
 // Relatórios
 Route::get('/relatorio/livros-por-autor', [ReportController::class, 'livrosPorAutor'])->name('report.livros-por-autor');
