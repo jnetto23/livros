8b74acfc1f1b7bdd5e01028fb4002e2759d56d8e
Joao Netto <jnetto@fyyb.com.br>
2025-11-06 11:50:27 -0300

    refactor: rotas

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\HomePage;
 use App\Http\Controllers\ReportController;
 
-// "/" redireciona para /subjects (opcional)
-Route::redirect('/', '/livros');
-
-// Rotas amigáveis, todas apontando pro mesmo componente, mudando "section"
-Route::get('/assuntos', HomePage::class)->name('subjects')->defaults('section', 'subjects');
-Route::get('/autores',  HomePage::class)->name('authors')->defaults('section', 'authors');
-Route::get('/livros',    HomePage::class)->name('books')->defaults('section', 'books');
-
-// Relatórios
 Route::get('/relatorio/livros-por-autor', [ReportController::class, 'livrosPorAutor'])->name('report.livros-por-autor');
+Route::get('/', HomePage::class)->name('home');
